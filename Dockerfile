a0d596fbf1905e2d12d12d9094992027941d4e8e
André Nishimoto <andre.nishimoto97@gmail.com>
2023-11-01 21:36:47 -0300

    att gitignore, dockerfile and database configs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,17 @@
 FROM node:20-alpine3.18
 
-WORKDIR /usr/app
+# Copie o package.json e o pnpm-lock.yaml
+COPY package*.json ./
 
-EXPOSE 9000
+# Instale as dependências do projeto
+RUN npm install
+
+# Copie os arquivos locais para o container
+COPY . /app
+
+# Defina o diretório de trabalho no container
+WORKDIR /app
+
+EXPOSE 8080
 
 CMD ["npm", "run", "start"]
--- src/outbound/database/orm/sequelize/model/index.js
+++ src/outbound/database/orm/sequelize/model/index.js
@@ -3,18 +3,12 @@
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
-const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'DEV';
-const config = require(__dirname + '/../config.json')[env];
 const db = {};
+const { defaultConnection : config} = require('../connection/index');
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+sequelize = new Sequelize(config);
 
 fs
   .readdirSync(__dirname)
